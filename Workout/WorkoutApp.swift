4c0697a24b79e0966c78ce25c654a0b1919d0bdf
AykutIpek <aykutipek_2001@hotmail.com>
2024-08-18 20:04:38 +0300

    Initial Commit

---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by aykut ipek on 18.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
